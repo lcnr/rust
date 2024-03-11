1bc52da541eb013b91910f92781f76a7ecccab2b
lcnr <rust@lcnr.de>
2024-03-11 10:57:08 +0100

    fix test

--- tests/ui/traits/next-solver/member-constraints-in-root-universe.rs
+++ tests/ui/traits/next-solver/member-constraints-in-root-universe.rs
@@ -1,16 +1,15 @@
 //@ compile-flags: -Znext-solver
-
+//@ check-pass
 trait Trait {
     type Ty;
 }
 
-impl Trait for for<'a> fn(&'a u8, &'a u8) {
+impl Trait for for<'a, 'b> fn(&'a u8, &'b u8) {
     type Ty = ();
 }
 
 // argument is necessary to create universes before registering the hidden type.
 fn test<'a>(_: <fn(&u8, &u8) as Trait>::Ty) -> impl Sized {
-    //~^ ERROR the type `<for<'a, 'b> fn(&'a u8, &'b u8) as Trait>::Ty` is not well-formed
     "hidden type is `&'?0 str` with '?0 member of ['static,]"
 }
 
