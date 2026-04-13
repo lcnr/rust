cf16dc5f31c2ebaee57507292d74874606e5c691
Jana Dönszelmann <jana@donsz.nl>
2026-04-13 13:37:51 +0200

    type safety for typing mode outside trait solver which can't be erased-not-coherence

--- compiler/rustc_trait_selection/src/solve/fulfill.rs
+++ compiler/rustc_trait_selection/src/solve/fulfill.rs
@@ -275,7 +275,7 @@ where
         &mut self,
         infcx: &InferCtxt<'tcx>,
     ) -> PredicateObligations<'tcx> {
-        let stalled_coroutines = match infcx.typing_mode() {
+        let stalled_coroutines = match infcx.typing_mode_raw().assert_not_erased() {
             TypingMode::Analysis { defining_opaque_types_and_generators } => {
                 defining_opaque_types_and_generators
             }
@@ -283,7 +283,6 @@ where
             | TypingMode::Borrowck { defining_opaque_types: _ }
             | TypingMode::PostBorrowckAnalysis { defined_opaque_types: _ }
             | TypingMode::PostAnalysis => return Default::default(),
-            TypingMode::ErasedNotCoherence => todo!(),
         };
 
         if stalled_coroutines.is_empty() {
